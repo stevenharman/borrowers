18d7f5416f6135925fc8c1b122de3909fe08f5d0
Steven Harman <steven@harmanly.com>
2014-12-11 17:09:06 -0500

    Add friends, including adding them.

---
+++ app/routes/friends.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    save: function() {
+      console.log('+--- save action bubbled up to the friends route');
+
+      return true;
+    },
+    cancel: function() {
+      console.log('+--- cancel action bubbled up to the friends route');
+
+      return true;
+    }
+  }
+});
---
+++ app/routes/friends/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('friend');
+  },
+  actions: {
+    save: function() {
+      console.log('+-- save action bubbled up to the friends new route');
+
+      return true;
+    },
+    cancel: function() {
+      console.log('+-- cancel action bubbled up to the friends new route');
+
+      return true;
+    }
+  }
+});
