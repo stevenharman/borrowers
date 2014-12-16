8ad32632f2e37f56b3380438f1417db3c738acd4
Steven Harman <steven@harmanly.com>
2014-12-15 20:31:38 -0500

    Remove unsaved articles and friends

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,14 +4,23 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
   },
+  activate: function() {
+    console.log("----- activate hook called -----");
+  },
+  deactivate: function() {
+    var model = this.modelFor('friends/new');
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
   actions: {
     save: function() {
-      console.log('+-- save action bubbled up to the friends new route');
+      console.log("+-- save action bubbled up to the friends new route");
 
       return true;
     },
     cancel: function() {
-      console.log('+-- cancel action bubbled up to the friends new route');
+      console.log("+-- cancel action bubbled up to the friends new route");
 
       return true;
     }
