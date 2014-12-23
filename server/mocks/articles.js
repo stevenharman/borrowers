819e235c866d0364c9fe1df5c335eecd38af210a
Steven Harman <steven@harmanly.com>
2014-12-23 17:16:52 -0500

    Add fake API resource for /api/articles/*

---
+++ server/mocks/articles.js
@@ -0,0 +1,36 @@
+module.exports = function(app) {
+  var express = require('express');
+  var articlesRouter = express.Router();
+
+  articlesRouter.get('/', function(req, res) {
+    res.send({
+      "articles": []
+    });
+  });
+
+  articlesRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  articlesRouter.get('/:id', function(req, res) {
+    res.send({
+      "articles": {
+        "id": req.params.id
+      }
+    });
+  });
+
+  articlesRouter.put('/:id', function(req, res) {
+    res.send({
+      "articles": {
+        "id": req.params.id
+      }
+    });
+  });
+
+  articlesRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/articles', articlesRouter);
+};
