412163b0722eb3a42588d899ab1f30edbfffe7e4
Steven Harman <steven@harmanly.com>
2014-12-23 16:57:06 -0500

    Add real test for date helpers

--- tests/unit/models/article-test.js
+++ tests/unit/models/article-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleForModel('article', 'Article', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:friend']
 });
 
 test('it exists', function() {
