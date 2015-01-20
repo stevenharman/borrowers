f846bf1b964d8bdf1e43936e66474fcd691a0d95
Steven Harman <steven@harmanly.com>
2015-01-19 21:52:41 -0500

    Tests still fail b/c "options" is undefined.
    I think this is an issue with the HTMLBars/Handlebars/Ember-CLI versions
    I'm on. I'll try upgrading to latest and see what happens.

--- tests/unit/helpers/formatted-date-test.js
+++ tests/unit/helpers/formatted-date-test.js
@@ -1,11 +1,9 @@
-import {
-  formattedDate
-} from 'borrowers/helpers/formatted-date';
+import formattedDate from 'borrowers/helpers/formatted-date';
 
 module('FormattedDateHelper');
 
 // Replace this with your real tests.
-test('it works', function() {
-  var result = formattedDate(42);
+test("it works", function() {
+  var result = formattedDate(42, "LL");
   ok(result);
 });
