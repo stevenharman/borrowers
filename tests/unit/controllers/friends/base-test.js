f8b446f205a410690d1b49315b8ff06bcdac62e7
Steven Harman <steven@harmanly.com>
2015-01-23 16:48:19 -0500

    Reorganize "Friends" resources as a POD.

--- tests/unit/controllers/friends/base-test.js
+++ tests/unit/controllers/friends/base-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('controller:friends/base', 'FriendsBaseController', {
+moduleFor('base-controller:friends', 'FriendsBaseController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
