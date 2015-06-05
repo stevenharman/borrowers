ff2776a0af5a3e8c3bce4d3dd01ec0a27c2a1569
Steven Harman <steven@harmanly.com>
2015-06-05 16:54:17 -0400

    Enable deleting of bad friends

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -9,6 +9,12 @@ export default Ember.Route.extend({
     cancel: function() {
       console.log('+--- cancel action bubbled up to friends route');
       return true;
+    },
+    delete: function(friend) {
+      var _this = this;
+      friend.destroyRecord().then(function() {
+        _this.transitionTo('friends.index');
+      });
     }
   }
 });
