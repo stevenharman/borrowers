ca8d873fd356dab9a354febb63380593a02805ec
Steven Harman <steven@harmanly.com>
2015-06-05 16:28:23 -0400

    Add Friend show page

--- app/router.js
+++ app/router.js
@@ -6,9 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('friends', function() {
+  this.route('friends', function() {
     this.route('new');
+    this.route('show', { path: ':friend_id' });
   });
+
 });
 
 export default Router;
