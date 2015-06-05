b3138282070f32702c26a3c449375d9aa11f1194
Steven Harman <steven@harmanly.com>
2015-06-05 14:15:29 -0400

    RESET for 2nd attempt

--- app/models/article.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  description: DS.attr('string'),
-  createdAt: DS.attr('date'),
-  friend: DS.belongsTo('friend'),
-  state: DS.attr('string', { defaultValue: 'borrowed' }),
-  notes: DS.attr('string')
-});
--- app/utils/date-helpers.js
+++
@@ -1,7 +0,0 @@
-function formatDate(date, format) {
-  return window.moment(date).format(format);
-}
-
-export {
-  formatDate
-};
