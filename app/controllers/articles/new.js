e9b8ab71f9c38c2020a4842e4238aad7f2bcc989
Steven Harman <steven@harmanly.com>
2015-06-12 15:52:54 -0400

    Allow Friends to borrow an Article

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  hasDescription: Ember.computed.notEmpty('description'),
+  hasNotes: Ember.computed.notEmpty('notes'),
+  isValid: Ember.computed.and('hasDescription', 'hasNotes'),
+
+  actions: {
+    save: function() {
+      if (this.get('isValid')) {
+        return true;
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+
+        return false;
+      }
+    }
+  }
+});
