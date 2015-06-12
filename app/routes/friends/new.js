9a52f9924dc4c40282d6a12bdc92a5a17e38470e
Steven Harman <steven@harmanly.com>
2015-06-12 16:52:25 -0400

    Cleanup unsaved Friends

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -5,6 +5,13 @@ export default Ember.Route.extend({
     return this.store.createRecord('friend');
   },
 
+  deactivate: function() {
+    var model = this.modelFor('friends/new');
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
+
   actions: {
     save: function() {
       console.log('+-- save action bubbled up to friends new route');
